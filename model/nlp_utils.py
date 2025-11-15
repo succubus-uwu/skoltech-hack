50962a30ae3ea3a39fa5fcb3d1f4110a8d15adcd
Лузин Борис Евгеньевич <luzin@tochka.com>
2025-11-15 21:31:20 +0300

    фыв

--- model/nlp_utils.py
+++ model/nlp_utils.py
@@ -1,6 +1,6 @@
 import spacy
 
-nlp = spacy.load("ru_core_news_sm")
+nlp = spacy.blank("ru")
 test_doc = nlp.make_doc(input())
 for token in test_doc:
     print(f"'{token.text}' [{token.idx}:{token.idx + len(token.text)}]")
