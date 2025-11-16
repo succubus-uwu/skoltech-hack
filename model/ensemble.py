47100bf221cd98c06f6d76a8098d3b6fc451f486
Лузин Борис Евгеньевич <luzin@tochka.com>
2025-11-16 11:39:11 +0300

    asd

--- model/ensemble.py
+++ model/ensemble.py
@@ -9,7 +9,7 @@ from model.new_ner import NERTrainer
 class Ensamble:
     def __init__(self, validator: Validator):
         self.validator = validator
-        self.ner_extractor = NERTrainer.load_from_path("/app/model")
+        self.ner_extractor = NERTrainer.load_from_path("/app/model_data")
 
     def process_text(self, text: str) -> dict:
         topos: list[dict] = self.ner_extractor.predict([text])
